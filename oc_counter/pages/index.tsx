8ce2528f77a4afdd2a4743edc79a762733ea92b5
Agnes Marie Ødegård Ockernahl <amockern@stud.ntnu.no>
2022-03-31 11:26:47 +0200

    deleted commented out code, and fixed a little bit on styling

--- oc_counter/pages/index.tsx
+++ oc_counter/pages/index.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from 'next'
-import styled from 'styled-components'
+import styled, { AnyStyledComponent } from 'styled-components'
 import BasicLayout from '../layout/Basic'
-//import FileInput, { Result } from 'react-file-reader-input'
 import FileInput, { Result } from 'react-file-reader-input'
 import { useEffect, useState, useRef } from 'react'
-//var sizeOf = require('image-size')
 
 /* Redux */
 import { connect } from 'react-redux'
@@ -16,12 +14,6 @@ import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '../store/store'
 
 import * as tf from '@tensorflow/tfjs'
-import * as automl from '@tensorflow/tfjs-automl'
-//const tfn = require('@tensorflow/tfjs-node')
-import modelAPI from './api/model'
-import { blob } from 'node:stream/consumers'
-import * as mobilenet from '@tensorflow-models/mobilenet'
-import { arrayBuffer } from 'stream/consumers'
 
 const Title = styled.h1`
   color: red;
@@ -33,11 +25,11 @@ const UploadCover = styled.div`
   height: 80vh;
   width: 50%;
   margin: auto;
-  background-color: #d439b2;
-  //background-color: #f5f6fa;
+  //background-color: #d439b2;
+  background-color: #f5f6fa;
   padding: 15.5px;
   border-radius: 8px;
-  grid-template-rows: 60% 40% 5%;
+  grid-template-rows: 30% 65% 5%;
 
   @media screen and(max-width: 768px) {
     width: 99%;
@@ -58,7 +50,7 @@ const UploadCover = styled.div`
   }*/
 `
 const UploadTop = styled.div`
-  height: 60%;
+  height: 80%;
   border: 2px solid #c2cdda;
   width: 100%;
   border-style: dashed;
@@ -68,20 +60,20 @@ const UploadTop = styled.div`
   align-items: center;
   padding: 2em;
   cursor: pointer;
-  background-color: #d46539;
+  //background-color: #d46539;
 `
 const UploadBottom = styled.div`
-  height: 30%;
+  height: 95%;
   width: 100%;
   overflow-y: scroll;
-  background-color: #c7eb48;
+  //background-color: #c7eb48;
 
   /*.Lists {
     height: ;
   }*/
 `
 const Lists = styled.div`
-  background-color: #cc3ab9;
+  //background-color: #cc3ab9;
   //height: 500px;
   //display: flex;
   //align-items: center;
@@ -93,21 +85,20 @@ const Boxes = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(10px, 1fr));
   padding-top: 1.2em;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   flex-wrap: wrap;
-  background-color: #39cfd4;
+  //background-color: #39cfd4;
 `
 const Box = styled.div`
-  width: 18vw; //height: 1.2em;
-  height: auto;
+  //width: 18vw; //height: 1.2em;
+  //height: auto;
   margin: 0.5vw;
-  background-color: #64e24b;
+  //background-color: #64e24b;
 `
 const SendButton = styled.div`
   color: white;
-  height: 10%;
-  background-color: #607878;
-  height: auto;
+  height: 100%;
+  //background-color: #d4d4d4;
   width: auto;
   cursor: pointer;
   font-family: 'Source Sans Pro', sans-serif;
@@ -118,14 +109,31 @@ const CardImage = styled.img`
   //display: flex;
   //align-items: center;
   //justify-content: center;
-  //width: 116px;
-  //height: 107px;
+  position: absolute;
+  //left: 0;
+  //right: 0;
+  width: 416px;
+  height: 416px;
+  z-index: 8;
   //object-fit: scale-down;
   //width: 100%;
   //height: auto;
   border-radius: 5px 5px 5px 5px;
 `
+const Canvas = styled.canvas`
+  position: absolute;
+  //marginLeft: 'auto';
+  //marginRight: 'auto';
+  //left: 0;
+  //right: 0;
+  z-index: 9;
+  // textAlign: 'center';
+  //zIndex: 8;
+  width: 416;
+  height: 416;
 
+  //background-color: red;
+`
 // Typescript
 /**
  * interface Props {
@@ -161,6 +169,7 @@ const Home: NextPage = (): JSX.Element => {
 
   const [model, setModel] = useState<tf.GraphModel | null>(null) //mobilenet.MobileNet  | automl.ObjectDetectionModel
   const imageRef = useRef<HTMLImageElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   /* 
   model: tf.Model;
@@ -168,11 +177,8 @@ const Home: NextPage = (): JSX.Element => {
 
   //Load model method
   const loadModel = async () => {
-    //const handler = tfn.io.fileSystem('../public/model.json')
     const model = await tf.loadGraphModel(WEIGHTS)
-    //const model = await automl.loadObjectDetection(WEIGHTS)
-    //console.log(model.summary())
-    //const model = await mobilenet.load()
+
     setModel(model)
   }
   useEffect(() => {
@@ -189,75 +195,33 @@ const Home: NextPage = (): JSX.Element => {
   }
 
   //make file to ImageData instead
-  const predict = async (
-    imageData: HTMLImageElement,
-    model: tf.GraphModel //mobilenet.MobileNet| automl.ObjectDetectionModel
-  ) => {
+  const predict = async (imageData: HTMLImageElement, model: tf.GraphModel) => {
     console.log(model)
     var imageHeight = imageData.height
-    /*console.log(imageHeight)
-    imageHeight = imageData.clientHeight
-    console.log(imageHeight)
-    imageHeight = imageData.offsetHeight
-    console.log(imageHeight)
-    imageHeight = imageData.naturalHeight
-    console.log(imageHeight)*/
+
     var image = imageRef.current
+    var canvas = canvasRef.current
+
     if (image) {
       const newImg = new Image(image.naturalWidth, image.naturalHeight)
       newImg.src = image.src
-      /*console.log(image.height)
-      console.log(image.clientHeight)
-      console.log(image.offsetHeight)
-      console.log(image.scrollHeight)
-      console.log(image.naturalHeight)
-      console.log(image.width)
-      console.log(image.clientWidth)
-      console.log(image.offsetWidth)
-      console.log(image.scrollWidth)
-      console.log(image.naturalWidth)*/
-      //image.width = image.naturalWidth
-      //image.height = image.naturalHeight
-
-      console.log(image.naturalHeight)
-      console.log(image.naturalWidth)
 
       const img = tf.browser.fromPixels(image, 3)
 
       console.log('Bilde')
-      //console.log(img)
-      //console.log(img.shape)
-      //console.log(img.size)
+
       const tfimg = img.transpose([0, 1, 2]).expandDims()
-      //console.log('Bilde2')
-      //console.log(tfimg)
+
       const resized = img.cast('float32').expandDims(0) //tf.image.resizeBilinear(img, [416, 416]) //.cast('float32').expandDims(0)
       const batchedImage = resized.div(tf.scalar(255))
-      //const prediction = await model.predict(resized) does not work
-      //const pred = await model.classify(resized)
-      //console.log(pred)
 
-      //var obj = await model.executeAsync(batchedImage)
       const input = cropImage(img).expandDims(0)
       const input2 = input.toFloat().div(tf.scalar(255))
       const obj = await model.executeAsync(input2)
 
-      //let conv_dims = obj.shape.slice(1,3)
       if (obj instanceof tf.Tensor) {
-        console.log('HEEEEEEEEER')
         let convDims = obj.shape.slice(1, 3)
 
-        /*const pred = boxes.filter(function (value, index, arr) {
-          return (index + 1) % 5 == 0
-        })
-        for (let index = 0; boxes.length; index++) {
-          if ((index + 1) % 5 == 0) {
-          } else {
-            boxes.append
-          }
-        }*/
-        //console.log(boxes)
-        //console.log(pred)
         var boxes = new Array()
         var pred = new Array()
 
@@ -275,7 +239,7 @@ const Home: NextPage = (): JSX.Element => {
 
         getBoxesAndPred(obj.dataSync())
         const tensor = tf.tensor2d(boxes, [boxes.length / 4, 4])
-        console.log(tensor.arraySync())
+
         console.log(boxes, pred)
         const selected_indices = tf.image.nonMaxSuppression(
           tensor,
@@ -285,55 +249,79 @@ const Home: NextPage = (): JSX.Element => {
           0.26
         )
         const selected_boxes = tf.gather(tensor, selected_indices)
-        console.log(selected_indices.print(), selected_boxes.print())
-        /*
-        const getEveryNth = (
-          arr: Uint8Array | Float32Array | Int32Array,
-          rowLength: number,
-          colIdx: number
-        ) => arr.filter((_, i) => i % rowLength === colIdx)
-*/
-        //console.log(getEveryNth(obj.dataSync(), 5, 4))
-        console.log(obj.dataSync())
+
+        const scores = pred[selected_indices.arraySync()[0]]
+        boxes = [selected_boxes.arraySync()[0]]
+        if (canvas) {
+          console.log('canvas')
+          const ctx = canvas.getContext('2d')
+          if (ctx) {
+            requestAnimationFrame(() => {
+              drawRect(boxes, scores, 0.2, 416, 416, ctx)
+            })
+          }
+        }
       } else {
         console.log('dette er den andre ')
         console.log(obj[0].dataSync())
       }
-      console.log('ikke i if sentence')
-      console.log(obj)
-
-      /*const obj = await model.executeAsync(resized, [
-        'detection_boxes',
-        'detection_scores',
-        'detection_classes',
-        'num_detections',
-      ])
-      //console.log(prediction)*/
-      //console.log('Model')
-      //console.log(obj)
-
-      //delete any existing tensors
 
       tf.dispose(img)
       tf.dispose(resized)
       tf.dispose(obj)
-      /*
-      if (model instanceof automl.ObjectDetectionModel) {
-        const options = { score: 0.5, iou: 0.5, topk: 20 }
-        const predictions = await model.detect(image, options)
-        console.log(predictions)
-      }*/
+    }
+  }
+
+  const drawRect = (
+    boxes: any,
+    //classes: any,
+    scores: any,
+    threshold: any,
+    imgWidth: any,
+    imgHeight: any,
+    ctx: CanvasRenderingContext2D //canvas
+  ) => {
+    console.log('drawrect')
+    console.log(boxes.length)
+    console.log(scores)
+    for (let i = 0; i < boxes.length; i++) {
+      console.log('for loop')
+      if (boxes[i] && scores > threshold) {
+        console.log('draw')
+        //ensure valid detection
+        //set variables
+        const [y, x, height, width] = boxes[i]
+        const text = 'Osteoclast'
+
+        //set styling
+        ctx.strokeStyle = 'red'
+        ctx.lineWidth = 5
+        ctx.fillStyle = 'white'
+        ctx.font = '20px Arial'
+
+        //draw
+        ctx.beginPath()
+        ctx.fillText(
+          text + ' - ' + Math.round(scores * 100) / 100,
+          x * imgWidth,
+          y * imgHeight - 10
+        )
+        ctx.rect(
+          x * imgWidth,
+          y * imgHeight,
+          (width * imgWidth) / 2,
+          (height * imgHeight) / 1.5
+        )
+        ctx.stroke()
+      }
     }
   }
 
   const detect = async () => {
-    const image = mediaFiles[0]
-    //console.log(image)
+    const image = mediaFiles[0] //funker ikke med predict
     var url = URL.createObjectURL(image)
     const im = new Image()
     im.src = url
-    //console.log(im.width)
-    //console.log(im.naturalWidth)
     if (model) {
       predict(im, model)
     }
@@ -363,7 +351,7 @@ const Home: NextPage = (): JSX.Element => {
 
   }
   */
-
+  //<img src="/upload.svg" />
   return (
     <BasicLayout>
       <Title> OC Counter </Title>
@@ -374,21 +362,15 @@ const Home: NextPage = (): JSX.Element => {
             onChange={handleChange}
             multiple={true}
             style={{ width: '100%', height: '100%' }}
-          >
-            <img src="/upload.svg" />
-          </FileInput>
+          ></FileInput>
         </UploadTop>
         <UploadBottom>
           <Lists>
             <Boxes>
               {mediaFiles.map((file: File) => (
                 <Box key={file.name}>
-                  <CardImage
-                    src={URL.createObjectURL(file)}
-                    ref={imageRef}
-                    width={416}
-                    height={416}
-                  />
+                  <CardImage src={URL.createObjectURL(file)} ref={imageRef} />
+                  <Canvas ref={canvasRef} />
                 </Box>
               ))}
             </Boxes>
